a47e30e511637e0c800695298ad8f1faad0bbec6
Nour El Houda Sarmout <nourelhoudasarmout@gmail.com>
2021-04-14 23:53:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -35,10 +35,8 @@ let routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog
 function exerciseDog(dogName, dogBreed){
   console.log("start");
   let result;
-  console.log(result);
   while (routine.length){
       result = routine.shift().call(this,dogName, dogBreed);
-      console.log(result);
    }
    console.log(result);
    return result;
